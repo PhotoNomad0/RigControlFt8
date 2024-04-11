33b8fec6f4a6ca8f674e5765868a98b34ea31fe6
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-04-11 15:50:30 -0400

    modify settings

--- frequencyControl.py
+++ frequencyControl.py
@@ -9,7 +9,23 @@ def get_json_from_url(url):
         print(f"An error occurred while trying to get data from the URL: {e}")
         return None
 
-# Use the function
-url = "http://127.0.0.1:8091/sdrangel/deviceset/0/device/settings"
-json_data = get_json_from_url(url)
-print(json_data)
+def getAdsAngelSettings():
+    url = "http://127.0.0.1:8091/sdrangel/deviceset/0/device/settings"
+    json_data = get_json_from_url(url)
+    return json_data
+
+def setAdsAngelFrequency(newFreq):
+    settings = getAdsAngelSettings()
+    print(settings)
+    
+    if settings:
+        newSettings = settings['rtlSdrSettings']['centerFrequency']
+        print(newSettings)
+        return newSettings
+
+    return None
+
+settings = setAdsAngelFrequency(14074000)
+print('new angel settings:')
+print(settings)
+
